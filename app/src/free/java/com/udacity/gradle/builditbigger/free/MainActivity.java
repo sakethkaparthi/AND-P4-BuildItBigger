1cbc580d21be0adfc87dfbe4ad91ee7c9c2a97b1
sakethkaparthi <sakethkaparthi@outlook.com>
2016-08-15 12:25:02 +0530

    strings

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
 
         requestNewInterstitial();
     }
