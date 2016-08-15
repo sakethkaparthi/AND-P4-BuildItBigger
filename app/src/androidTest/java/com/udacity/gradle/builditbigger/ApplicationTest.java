a4eb2b167a99f6e743c1e1c9ffd93ce3059be557
sakethkaparthi <sakethkaparthi@outlook.com>
2016-08-15 11:42:28 +0530

    Tests

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,31 @@
 package com.udacity.gradle.builditbigger;
 
 import android.app.Application;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Test
+    public void testJoker() throws Exception {
+        new AsyncTest(new AsyncTest.OnJokeFetchedListener() {
+            @Override
+            public void onJokeFetched(String joke) {
+                assertNotNull(joke);
+                assertTrue(joke.length() > 0);
+            }
+        }).execute();
     }
 }
--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTest.java
@@ -1,45 +1,47 @@
+package com.udacity.gradle.builditbigger;
+
 import android.os.AsyncTask;
 
 import com.example.saketh.myapplication.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 
-import org.junit.Test;
-
 import java.io.IOException;
 
 /**
  * Created by saketh on 14/8/16.
  */
-public class AsyncTest {
-    @Test
-    public void nonEmptyResponseTest() {
-        EndpointsAsyncTask asyncTask = new EndpointsAsyncTask();
-        asyncTask.execute();
+public class AsyncTest extends AsyncTask<Void, Void, String> {
+    private OnJokeFetchedListener listener;
+    private MyApi myApiService = null;
+
+    public AsyncTest(OnJokeFetchedListener listener) {
+        this.listener = listener;
     }
 
-    class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
-        private MyApi myApiService = null;
+    @Override
+    protected String doInBackground(Void... params) {
+        if (myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://build-it-bigger-140314.appspot.com/_ah/api/");
+            // end options for devappserver
 
-        @Override
-        protected String doInBackground(Void... params) {
-            if (myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                        .setRootUrl("https://build-it-bigger-140314.appspot.com/_ah/api/");
-                // end options for devappserver
-
-                myApiService = builder.build();
-            }
-            try {
-                return myApiService.fetchJoke().execute().getData();
-            } catch (IOException e) {
-                return e.getMessage();
-            }
+            myApiService = builder.build();
         }
-
-        @Override
-        protected void onPostExecute(String result) {
-            assert result.length() > 0;
+        try {
+            return myApiService.fetchJoke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
         }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        listener.onJokeFetched(result);
+    }
+
+    public interface OnJokeFetchedListener {
+        void onJokeFetched(String joke);
     }
 }
+
