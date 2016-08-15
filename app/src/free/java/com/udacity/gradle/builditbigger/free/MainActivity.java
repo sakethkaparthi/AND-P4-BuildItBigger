3b08f82cc37ca4569f1e86ad5b15544ae068afba
sakethkaparthi <sakethkaparthi@outlook.com>
2016-08-15 10:59:07 +0530

    Interstitial ads

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -8,17 +8,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.R;
 
 import sakethkaparthi.androidjoker.JokeDisplayActivity;
 
 public class MainActivity extends AppCompatActivity {
+    InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        requestNewInterstitial();
     }
 
 
@@ -50,11 +58,30 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.show();
         EndpointsAsyncTask asyncTask = new EndpointsAsyncTask(new EndpointsAsyncTask.OnFetchCompleted() {
             @Override
-            public void onFetched(String result) {
+            public void onFetched(final String result) {
+                mInterstitialAd.setAdListener(new AdListener() {
+                    @Override
+                    public void onAdClosed() {
+                        requestNewInterstitial();
+                        startActivity(new Intent(MainActivity.this, JokeDisplayActivity.class).putExtra("joke", result));
+                    }
+                });
                 progressDialog.dismiss();
-                startActivity(new Intent(MainActivity.this, JokeDisplayActivity.class).putExtra("joke", result));
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    startActivity(new Intent(MainActivity.this, JokeDisplayActivity.class).putExtra("joke", result));
+                }
             }
         });
         asyncTask.execute();
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
     }
 }
